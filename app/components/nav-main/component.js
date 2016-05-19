a4f87ad5c2b8837dbbc54d5189c4c6aa550f5063
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-05-19 18:01:18 +0200

    add sub navigation on mousover

--- app/components/nav-main/component.js
+++ app/components/nav-main/component.js
@@ -6,15 +6,50 @@ export default Component.extend({
 	layout: inject.service(),
 	classNames: ['Header'],
 
-	detail: on('afterRender', function() {
+	detail: on('didInsertElement', function() {
 		// $('.Nav').sticky();
+		this.actions();
 	}),
 
+	// open subNav
+	open(event) {
+		console.log('open');
+		event.preventDefault();
+
+		const dropdown = $('.Nav--sub');
+
+		$(dropdown).removeClass('is-hidden');
+
+		$('.Nav--sub').on('mouseleave', event => this.close(event));
+	},
+
+	// close subNav Dropdown
+	close(event) {
+		console.log('close');
+		event.preventDefault();
+
+		const dropdown = $('.Nav--sub');
+
+		$(dropdown).addClass('is-hidden');
+	},
+
+	actions() {
+		this.$subNav = $('.js-openSub');
+
+		$('.Nav a').on('click', function() {
+			console.log('clicked');
+		})
+
+		this.$subNav.on('mouseenter', event => this.open(event));
+	},
+
 	click(event) {
-		const clickedElementHasAnHref = event.target.href;
-		console.log(clickedElementHasAnHref);
-		if (clickedElementHasAnHref && this.get('layout.showAside')) {
-			this.get('layout').perhapsCloseAside();
-		}
+		const clickedElementHasAnHref = $(this);
+		// console.log(clickedElementHasAnHref);
+		// console.log(event);
+
+		// if (clickedElementHasAnHref && this.get('layout.showAside')) {
+		// 	this.get('layout').perhapsCloseAside();
+		// }
 	}
 });
