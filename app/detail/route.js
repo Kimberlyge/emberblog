105fbbf5f88f5c7bed9538d4e4acd875ac93eb0c
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-05-21 01:25:01 +0200

    overview and detail dynamic

--- app/detail/route.js
+++ app/detail/route.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		// return this.store.findRecord('post', 5);
+		// return Ember.RSVP.hash({
+		// 	// page: this.store.findRecord('page', 67)
+		// 	// artists: this.store.findAll('artist'),
+		// 	// news: this.store.query('post', {per_page: 2})
+
+		// 	// categories: this.store.findAll('category'),
+
+		// 	posts: this.store.findAll('post')
+		// });
+		return this.store.findRecord('post', 18);
+
 	}
 });
