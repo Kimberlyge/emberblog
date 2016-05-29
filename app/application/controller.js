ddff6fc61c8753b60d91b0a3e7f8e6ad5f427f86
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-05-29 12:43:16 +0200

    refator service / js / scss

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	layout: Ember.inject.service(),
-
-	// sticky: on('didInsertElement', function () {
-	// 	$('.Header').sticky();
-	// })
 });
--- app/layout/service.js
+++ app/layout/service.js
@@ -2,28 +2,29 @@
 import Ember from 'ember';
 // import Cookies from 'ember-cli-js-cookie';
 
-const BREAKPOINT = 740;
+// const BREAKPOINT = 740;
 const {computed, on} = Ember;
 
 export default Ember.Service.extend({
 	onMobile: false,
+	onTravel: false
 
-	setInitialWidth: on('init', function () {
-		const xBrowserWidth = document.querySelector('body').getBoundingClientRect().width;
-		this.set('initialWidth', xBrowserWidth);
-	}),
+	// setInitialWidth: on('init', function () {
+	// 	const xBrowserWidth = document.querySelector('body').getBoundingClientRect().width;
+	// 	this.set('initialWidth', xBrowserWidth);
+	// }),
 
 	// 513px is our current breakpoint for panel to be full width.
-	smallScreen: computed('initialWidth', function () {
-		return this.get('initialWidth') < BREAKPOINT;
-	}),
+	// smallScreen: computed('initialWidth', function () {
+	// 	return this.get('initialWidth') < BREAKPOINT;
+	// }),
 
 	// Only close if we're on a small screen.
-	perhapsCloseAside() {
-		console.log('perhapscloseaside');
-		if (this.get('smallScreen')) {
-			console.log('closing');
-			this.set('showAside', false);
-		}
-	}
+	// perhapsCloseAside() {
+	// 	console.log('perhapscloseaside');
+	// 	if (this.get('smallScreen')) {
+	// 		console.log('closing');
+	// 		this.set('showAside', false);
+	// 	}
+	// }
 });
