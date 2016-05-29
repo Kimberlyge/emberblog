5b7efcc972dc712d25869dd5b0b9ad46154cc1d4
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-05-29 05:38:13 +0200

    page single layout

--- app/travel/single/route.js
+++ app/travel/single/route.js
@@ -8,9 +8,6 @@ export default Ember.Route.extend({
 	},
 
 	model(params) {
-		// return this.store.findRecord('page', 101);
 		return this.store.findRecord('page', params.id);
-		// return this.store.query('page', {filter: {id: params.id}});
-		return this.get('id');
 	}
 });
