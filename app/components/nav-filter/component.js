258d1e3112f2d14d934067622f9c708b8a847487
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-06-04 21:14:51 +0200

    sticky header

--- app/components/nav-filter/component.js
+++ app/components/nav-filter/component.js
@@ -1,5 +1,23 @@
+// import Stickyjs from 'npm:flickity';
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-	classNames: ['NavFilter']
+const {Component, $, on} = Ember;
+
+export default Component.extend({
+	classNames: ['NavFilter'],
+
+	sticky: on('didRender', function() {
+		// $('.Nav').sticky(); @todo: not working for some reason
+		this.stick();
+		// console.log('yess');
+	}),
+
+	stick() {
+		// $(document).ready(function(){
+		// 	$(".NavFilter--left").sticky({topSpacing:0});
+		// });
+
+		// $('.NavFilter--left').sticky();
+
+	}
 });
