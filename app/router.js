4273e9a484f84b6e6b623573528d4b634a8df4fb
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-12-11 02:43:33 +0100

    add legal routes

--- app/router.js
+++ app/router.js
@@ -9,13 +9,12 @@ const Router = Ember.Router.extend(googlePageview, {
 Router.map(function () {
   this.route('index', {path: '/'});
   this.route('detail', {path: '/detail/:slug'});
-  this.route('travel', {path: 'travel'}, function () {
-  	// this.route('single', {path: '/:slug'}, function () {
-  	// });
-  });
+  this.route('travel', {path: 'travel'});
   this.route('city', {path: '/travel/:slug'});
 
   this.route('404', {path: '/*wildcard'});
+  this.route('about');
+  this.route('imprint');
 });
 
 export default Router;
